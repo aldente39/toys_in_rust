24810c9eb5c51b516d4e1c99c2883f8365db22ab
aldente39 <1268059+aldente39@users.noreply.github.com>
2021-11-22 22:22:47 +0900

    テスト整理

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,17 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use std::collections::LinkedList;
+
 pub fn execute_program(program: &String) -> i32 {
     let parsed = parser::parse(&program);
     let mut i = interpreter::Interpreter::new();
     i.call_main(&parsed)
+}
+
+pub fn execute_lines(lines: &String) -> i32 {
+    let parsed = parser::parse_lines(&lines);
+    let i = interpreter::Interpreter::new();
+    let results: LinkedList<i32> = parsed.iter().map(|x| -> i32 { i.interpret(x)} ).collect();
+    results.into_iter().last().unwrap_or_else(|| 0)
 }
--- tests/test.rs
+++ tests/test.rs
@@ -3,6 +3,7 @@ extern crate toys_in_rust;
 use toys_in_rust::parser::*;
 use toys_in_rust::interpreter::Interpreter;
 use toys_in_rust::execute_program;
+use toys_in_rust::execute_lines;
 
 use std::collections::LinkedList;
 use rstest::rstest;
@@ -11,80 +12,6 @@ use rstest::rstest;
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_add() {
-        let program = "define main() {61+50+9;}".to_string();
-        let parsed = parse(&program);
-        let mut i = Interpreter::new();
-        let result = i.call_main(&parsed);
-        assert_eq!(120, result);
-    }
-    #[test]
-    fn test_factorial() {
-        let program = r#"
-            define main() {
-                fact(5);
-            }
-            define fact(n) {
-                if (n < 2) {
-                    1;
-                } else {
-                    n*fact(n-1);
-                }
-            }
-        "#.to_string();
-        let parsed = parse(&program);
-        let mut i = Interpreter::new();
-        let result = i.call_main(&parsed);
-        assert_eq!(120, result);
-    }
-    #[test]
-    fn test_global() {
-        let program = r#"
-            global pi = 3;
-            define main() {
-                foo() * 3;
-            }
-            define foo() {
-                pi* 2*2;
-            }
-        "#.to_string();
-        let parsed = parse(&program);
-        let mut i = Interpreter::new();
-        let result = i.call_main(&parsed);
-        assert_eq!(36, result);
-    }
-    #[test]
-    fn test_while() {
-        let program = r#"
-            define main() {
-                i = 0;
-                while (i < 10) {
-                    i = i + 1;
-                }
-                i;
-            }
-        "#.to_string();
-        let parsed = parse(&program);
-        let mut i = Interpreter::new();
-        let result = i.call_main(&parsed);
-        assert_eq!(10, result);
-    }
-    #[test]
-    fn test_while2() {
-        let program = r#"
-            i = 0;
-            while (i < 10) {
-                i = i + 1;
-            }
-            i;
-        "#.to_string();
-        let parsed = parse_lines(&program);
-        let i = Interpreter::new();
-        let results: LinkedList<i32> = parsed.iter().map(|x| -> i32 { i.interpret(x)} ).collect();
-        let result = results.into_iter().last();
-        assert_eq!(10, result.unwrap());
-    }
     #[test]
     fn test_println() {
         let program = r#"
@@ -98,8 +25,52 @@ mod tests {
         assert_eq!(15, result.unwrap());
     }
     #[rstest(name, input, expected,
+        case("test_while_lines", r#"
+            i = 0;
+            while (i < 10) {
+                i = i + 1;
+            }
+            i;
+        "#, 10),
+        case("test_assignment", r#"
+            a = 1 + 2;
+            a = a + 3;
+            a = a + a;
+        "#, 12),
+        case("test_if1", r#"
+            if(1 >= 2) {
+                a = 1;
+            } else {
+                a = 0;
+            }
+        "#, 0),
+        case("test_if2", r#"
+            if(1 <= 2) {
+                a = 1;
+            } else {
+                a = 0;
+            }
+        "#, 1),
+        ::trace
+    )]
+    fn test_lines(name: String, input: String, expected: i32) {
+        assert_eq!(execute_lines(&input), expected);
+    }
+    #[rstest(name, input, expected,
         case("test_add", r#"
             define main() { 61+50+9; }
+        "#, 120),
+        case("test_factorial", r#"
+            define main() {
+                fact(5);
+            }
+            define fact(n) {
+                if (n < 2) {
+                    1;
+                } else {
+                    n*fact(n-1);
+                }
+            }
         "#, 120),
         case("test_while", r#"
             define main() {
@@ -110,6 +81,44 @@ mod tests {
                 i;
             }
         "#, 10),
+        case("test_global", r#"
+            global pi = 3;
+            define main() {
+                foo() * 3;
+            }
+            define foo() {
+                pi* 2*2;
+            }
+        "#, 36),
+        case("test_multiply", r#"
+            define main() {
+                ((3+4)*5+1)/2*2*3-5-4;
+            }
+        "#, 99),
+        case("test_define_function", r#"
+            define main() {
+                v = add2(v);
+            }
+            global v = 2;
+            define add2(x) {
+                x + 2;
+            }
+        "#, 4),
+        case("test_factorial2", r#"
+            define factorial(n) {
+                if(n < 2) {
+                    1;
+                } else {
+                    n * factorial(n - 1);
+                }
+            }
+            global n = 0;
+            define main() {
+                n = factorial(6);
+                println(n);
+                n;
+            }
+        "#, 720),
         ::trace
     )]
     fn test_program(name: String, input: String, expected: i32) {
